e4b26f7b48f9f6540b418b6253c490ed3299af36
gabomartin <75818899+gabomartin@users.noreply.github.com>
2022-02-24 15:51:23 -0300

    Adición de proyecto WPF

---
+++ BinaryConverterWPF/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BinaryConverterLogic;
+
+namespace BinaryConverterWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void ToBinarioText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+           
+        }
+
+        private void ToDecimalText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void ToBinarioButton_Click(object sender, RoutedEventArgs e)
+        { 
+            BinaryConverterLogic.Logic logic = new Logic();
+            long input = Convert.ToInt64(ToBinarioText.Text);
+            D2BResult.Text = logic.IntToBinary(input);
+        }
+
+        private void ToDecimalButton_Click(object sender, RoutedEventArgs e)
+        {
+            BinaryConverterLogic.Logic logic = new Logic();
+            long input = Convert.ToInt64(ToDecimalText.Text);
+            B2DResult.Text = logic.BinaryToInt(input).ToString();
+        }
+    }
+}
